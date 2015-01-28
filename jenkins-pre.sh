8eb3608d8612486d55efe8762d9b244296f557a0
v-rajan <vrajan@chnoumis.com>
2015-01-28 20:32:43 +1100

    Add configuration files and certificates

--- jenkins-pre.sh
+++ jenkins-pre.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+# Certificate installation
 cd /etc/jenkins/certs
 sudo cp -r * /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 
+# Maven installation
+cp /etc/jenkins/m2/settings.xml /var/jenkins_home/.m2/settings.xml
+ 	
 exec /usr/local/bin/jenkins.sh
