26f67ca372f81799a1908b1b28eba0eeb252d671
v-rajan <vrajan@chnoumis.com>
2015-02-05 00:10:12 +1100

    Create .m2 folder before copying setting.xml

--- jenkins-pre.sh
+++ jenkins-pre.sh
@@ -6,6 +6,7 @@ sudo cp -r * /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 
 # Maven installation
+mkdir -p /var/jenkins_home/.m2/
 cp /etc/jenkins/m2/settings.xml /var/jenkins_home/.m2/settings.xml
  	
 exec /usr/local/bin/jenkins.sh
